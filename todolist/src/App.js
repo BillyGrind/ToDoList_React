dc4860339ea66487fbf41dabd840f85a4b6e4d6d
BillyGrind <denayeranthon1782@gmail.com>
2023-08-14 11:53:52 +0200

    focus

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,90 +1,106 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { nanoid } from "nanoid";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 // Ex source
 //https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_interactivity_filtering_conditional_rendering
 
-
 export default function App(props) {
-
-  const[tasks,setTasks] = useState(props.tasks);
+  const [tasks, setTasks] = useState(props.tasks);
   const tasksNoun = tasks.length !== 1 ? "tasks" : "task";
   const headingText = `${tasks.length} tasks remaining`;
-  const [filter,setFilter]=useState("All");
+  const [filter, setFilter] = useState("All");
   const FILTER_MAP = {
     All: () => true,
     Active: (task) => !task.completed,
     Completed: (task) => task.completed,
   };
-  
   const FILTER_NAMES = Object.keys(FILTER_MAP);
-  const filterList = FILTER_NAMES.map((name)=>(
-    <FilterButton 
-    key={name} 
-    name={name}
-    isPressed={name===filter}
-    setFilter={setFilter}
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
-  ))
+  ));
+
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
 
   function addTask(name) {
-    const newTask = {id:`todo-${nanoid()}`,name,completed:false};
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     // console.log(newTask);
-    setTasks([...tasks,newTask]);
+    setTasks([...tasks, newTask]);
     // alert(name);
   }
 
-  function toggleTaskCompleted(id){
-    const updatedTasks = tasks.map((task)=>{
-      if(id===task.id){
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
         // console.log(task);
-        return{...task,completed:!task.completed}
+        return { ...task, completed: !task.completed };
       }
       return task;
     });
     setTasks(updatedTasks);
   }
 
-  function deletedTask(id){
-    const remainingTasks = tasks.filter((task)=> id != task.id);
+  function deletedTask(id) {
+    const remainingTasks = tasks.filter((task) => id != task.id);
     setTasks(remainingTasks);
     console.log(id);
   }
 
-  function editTask(id, newName){
-    const editedTaskList = tasks.map((task)=>{
-      if(id===task.id){
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
         console.log(task.id);
         return { ...task, name: newName };
       }
       return task;
-    })
+    });
     setTasks(editedTaskList);
   }
 
   const taskList = tasks
-  .filter(FILTER_MAP[filter])
-  .map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deletedTask={deletedTask}
-      editTask={editTask}
-    />
-  ));
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deletedTask={deletedTask}
+        editTask={editTask}
+      />
+    ));
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       {filterList}
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex={-1} ref={listHeadingRef}>
+        {headingText}{" "}
+      </h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- todolist/src/components/Todo.js
+++ todolist/src/components/Todo.js
@@ -1,9 +1,30 @@
-import React,{useState} from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 export default function Todo(props) {
+  const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState("");
 
-  const [isEditing,setEditing]=useState(false);
-  const [newName,setNewName]=useState("");
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+  const wasEditing = usePrevious(isEditing);
+
+  function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  }
+  
+ useEffect(() => {
+  if (!wasEditing && isEditing) {
+    editFieldRef.current.focus();
+  }
+  if (wasEditing && !isEditing) {
+    editButtonRef.current.focus();
+  }
+}, [wasEditing, isEditing]);
+
 
   const editingTemplate = (
     <form className="stack-small" onSubmit={handleSubmit}>
@@ -11,10 +32,21 @@ export default function Todo(props) {
         <label className="todo-label" htmlFor={props.id}>
           New name for {props.name}
         </label>
-        <input id={props.id} className="todo-text" type="text" value={newName} onChange={handleChange} />
+        <input
+          id={props.id}
+          className="todo-text"
+          type="text"
+          value={newName}
+          onChange={handleChange}
+          ref={editFieldRef}
+        />
       </div>
       <div className="btn-group">
-        <button type="button" className="btn todo-cancel" onClick={() => setEditing(false)}>
+        <button
+          type="button"
+          className="btn todo-cancel"
+          onClick={() => setEditing(false)}
+        >
           Cancel
           <span className="visually-hidden">renaming {props.name}</span>
         </button>
@@ -39,31 +71,36 @@ export default function Todo(props) {
         </label>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn" onClick={() => setEditing(true)}>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setEditing(true)}
+          ref={editButtonRef}
+        >
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
         <button
           type="button"
           className="btn btn__danger"
-          onClick={() => props.deletedTask(props.id)}>
+          onClick={() => props.deletedTask(props.id)}
+        >
           Delete <span className="visually-hidden">{props.name}</span>
         </button>
       </div>
     </div>
   );
-  
-    function handleChange(e){
-      setNewName(e.target.value)
-      // console.log("changement");
-    }
-    function handleSubmit(e) {
-      e.preventDefault();
-      props.editTask(props.id, newName);
-      // console.log(newName);
-      setNewName("");
-      setEditing(false);
-    }
-    
+
+  function handleChange(e) {
+    setNewName(e.target.value);
+    // console.log("changement");
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.editTask(props.id, newName);
+    // console.log(newName);
+    setNewName("");
+    setEditing(false);
+  }
 
   return (
     <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>
